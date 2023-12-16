01b6f8a1ac5cba34c7c52da5dc1e3c709cb2f5cf
Emily <143989797+sdhEmily@users.noreply.github.com>
2023-12-16 19:38:49 +0000

    add views and stuff

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+$.getJSON("https://api.countabc.xyz/hit/sdh.gay/visits", function(response) {
+    $("#views").text(response.value);
+});
+
 function discord() {
   navigator.clipboard.writeText('.emiily_');
   alert('Discord Username copied to clipboard.');
