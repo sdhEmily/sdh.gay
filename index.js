4da4eeb1a9e28423bf2ccc701f5713e93833f5e6
Emily <143989797+sdhEmily@users.noreply.github.com>
2023-12-16 16:07:02 +0000

    now its a feds.lol skid lol

---
+++ index.js
@@ -0,0 +1,9 @@
+function discord() {
+  navigator.clipboard.writeText('.emiily_');
+  alert('Discord Username copied to clipboard.');
+}
+
+function github() {
+    window.open('http://www.github.com/sdhEmily', '_blank').focus();
+}
+  
